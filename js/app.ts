955f57d1d3749be8a22e4ac4074ca193df372a30
Mariana Athayde <mariana.athayde@gmail.com>
2023-09-09 22:31:18 -0300

    Update app.ts

--- js/app.ts
+++ js/app.ts
@@ -1,18 +1,10 @@
 const buttons = document.querySelectorAll(".tecla");
 const display = document.getElementById("display") as HTMLSpanElement;
-const onButton = document.getElementById("on");
-const resultButton = document.getElementById("igual");
-const sumButton = document.getElementById("mas");
-const substractButton = document.getElementById("menos");
-const multiplyButton = document.getElementById("por");
-const divideButton = document.getElementById("dividido");
 
 let displayValue: string = "0";
 let previousValue: string;
 let operator: string | null = null;
 let waitingForSecondOperand: boolean = false;
-let decimalEntered: boolean = false;
-let result: number;
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -20,60 +12,47 @@ buttons.forEach((button) => {
 
         switch (buttonText) {
             case "on":
-                display.textContent = "0";
+                displayValue = "0";
                 break;
             case "mas":
-                previousValue = display.textContent;
-                operator = "+";
-                display.textContent = "+";
-                waitingForSecondOperand = true;
-                break;
             case "menos":
-                previousValue = display.textContent;
-                operator = "-";
-                display.textContent = "-";
-                waitingForSecondOperand = true;
-                break;
             case "por":
-                previousValue = display.textContent;
-                operator = "*";
-                display.textContent = "*";
-                waitingForSecondOperand = true;
-                break;
             case "dividido":
-                previousValue = display.textContent;
-                operator = "/";
-                display.textContent = "/";
+                operator = buttonText;
+                previousValue = displayValue;
                 waitingForSecondOperand = true;
                 break;
             case "igual":
                 if (waitingForSecondOperand) {
-                    let currentValue = parseFloat(display.textContent);
                     switch (operator) {
-                        case "+":
-                            result = parseFloat(previousValue) + currentValue;
+                        case "mas":
+                            displayValue = (parseFloat(previousValue) + parseFloat(displayValue)).toString();
                             break;
-                        case "-":
-                            result = parseFloat(previousValue) - currentValue;
+                        case "menos":
+                            displayValue = (parseFloat(previousValue) - parseFloat(displayValue)).toString();
                             break;
-                        case "*":
-                            result = parseFloat(previousValue) * currentValue;
+                        case "por":
+                            displayValue = (parseFloat(previousValue) * parseFloat(displayValue)).toString();
                             break;
-                        case "/":
-                            result = parseFloat(previousValue) / currentValue;
+                        case "dividido":
+                            if (parseFloat(displayValue) === 0) {
+                                displayValue = "Error";
+                            } else {
+                                displayValue = (parseFloat(previousValue) / parseFloat(displayValue)).toString();
+                            }
                             break;
                     }
-                    display.textContent = result.toString();
                     waitingForSecondOperand = false;
                 }
                 break;
             default:
-                if (display.textContent.length >= 8) return;
-                if (display.textContent === "0") {
-                    display.textContent = null;
+                if (displayValue === "0" || waitingForSecondOperand) {
+                    displayValue = buttonText!;
+                } else {
+                    displayValue += buttonText!;
                 }
-                display.textContent += buttonText!;
                 break;
         }
-    }) 
-})
+        display.textContent = displayValue;
+    });
+});
